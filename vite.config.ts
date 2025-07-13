db4312b1693b7eea011b3a5000f16bd783765858
An Nguyen <anndb1903@gmail.com>
2025-07-13 22:44:05 +0700

    config vite

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,21 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()]
+  plugins: [react(), tsconfigPaths()],
+  server: {
+    port: 5173
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        // api: "modern-compiler", // or "modern"
+        silenceDeprecations: [
+          "mixed-decls",
+          "color-functions",
+          "global-builtin",
+          "import"
+        ] // k hiện warning của thư viện gallery
+      }
+    }
+  }
 });
