a31ad53c88612edffb7ceb6f168244a9dad3de09
Nguyễn Đình Bảo Ân <anndb1903@gmail.com>
2025-11-06 21:34:47 +0700

    Update index.tsx

--- src/components/admin/dashboard/index.tsx
+++ src/components/admin/dashboard/index.tsx
@@ -17,7 +17,6 @@ import { getAdminStats } from "../../../services/adminApi";
 import { Pie } from "@ant-design/charts";
 
 const { Statistic } = StatisticCard;
-const { Option } = Select;
 
 interface AdminStats {
   totalUsers: number;
